d224171303d66c533878fbb63e8242ab41d4afcb
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-06-21 23:15:46 +0300

    clean & make scripts

---
+++ clean.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+rmdir /s /q .vs
+rmdir /s /q .idea
+
+for /f "delims=" %%e in ('dir /A:D /S /B *bin^|find /i "\bin"') do @if exist "%%e" (@rmdir /S /Q %%e)
+for /f "delims=" %%e in ('dir /A:D /S /B *obj^|find /i "\obj"') do @if exist "%%e" (@rmdir /S /Q %%e)
+for /f "delims=" %%e in ('dir /A:D /S /B *.vs^|find /i "\.vs"') do @if exist "%%e" (@rmdir /S /Q %%e)
